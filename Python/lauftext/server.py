b88cdc99e29c2b740e9c02353dc3f4e3212b5a83
Wunderbaeumchen99817 <katharinavolkenand@gmx.de>
2022-02-20 22:56:07 +0100

    korrekter Port hinzugefügt

--- Python/lauftext/server.py
+++ Python/lauftext/server.py
@@ -97,4 +97,4 @@ def my_form_post():
     return my_form(entry=retrieved_text)
 
 if(__name__ == "__main__"):
-    app.run(debug=False, host="0.0.0.0")
+    app.run(debug=False, host="0.0.0.0", port="80")
