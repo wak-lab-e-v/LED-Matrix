6239f879385378f337de96247b6bfd8ba7a5c375
Wunderbaeumchen99817 <51451724+Wunderbaeumchen99817@users.noreply.github.com>
2022-02-21 11:49:41 +0100

    start für Pong

--- Python/PixelServer/skripte/pong.py
+++ Python/PixelServer/skripte/pong.py
@@ -1,19 +1,32 @@
 from flask import request, render_template
+import matrix_handler
 
 class Pong():
 
-    def __init__(self):
-        pass
+    def __init__(self,matrix: matrix_handler.Matrix):
+        self.pixelflut = matrix
 
-    def post(self):
+    def send_command(self,command: str):
+        self.pixelflut.send_pong(command)
+
+    def receive_post(self):
         command = str(request.form['command'])
         side = str(request.form['side'])
-
+    
         if(command=="0"):
             #up
+            if(side=="helle"):
+                pass
+            elif(side=="dunkle"):
+                
+                pass
             pass
         elif(command=="1"):
             #down
+            if(side=="helle"):
+                pass
+            elif(side=="dunkle"):
+                pass
             pass
 
         return render_template('/pong/ingame/index.html', side=side)
