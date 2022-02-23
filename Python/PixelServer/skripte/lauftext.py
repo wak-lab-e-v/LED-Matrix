487b6e96b5f769b74d5e76d85fee88a58137cc02
Wunderbaeumchen <51451724+Wunderbaeumchen99817@users.noreply.github.com>
2022-02-23 11:52:07 +0100

    Reduzierung der Absturzgefahr

--- Python/PixelServer/skripte/lauftext.py
+++ Python/PixelServer/skripte/lauftext.py
@@ -24,74 +24,77 @@ class Lauftext():
 
     #@app.route('/lauftext', methods=['POST'])
     def post(self):
-        text = str(request.form['text'])
+        try:
+            text = str(request.form['text'])
+            print(text)
 
-        collected_input = text.split(" ", 1)
-        if(len(collected_input)>1):
-            retrieved_text = collected_input[1]
-            color_tag = collected_input[0]
-            #not using the new match feature from Python 3.10, since not everybody has it installed
-            if( color_tag == "red" 
-                or color_tag == "Red" 
-                or color_tag == "rot" 
-                or color_tag == "Rot" 
-                or color_tag == "ROT" 
-                or color_tag == "RED"):
-                color = self.standard_colors["red"]
-            elif(  color_tag == "green" 
-                    or color_tag == "Green" 
-                    or color_tag == "grün" 
-                    or color_tag == "Grün" 
-                    or color_tag == "GREEN" 
-                    or color_tag == "grun" 
-                    or color_tag == "GRÜN"):
-                color = self.standard_colors["green"]
-            elif(   color_tag == "blue" 
-                    or color_tag == "Blue" 
-                    or color_tag == "blau" 
-                    or color_tag == "Blau" 
-                    or color_tag == "BLUE" 
-                    or color_tag == "BLAU"):
-                color = self.standard_colors["blue"]
-            elif(   color_tag == "yellow" 
-                    or color_tag == "Yellow" 
-                    or color_tag == "gelb" 
-                    or color_tag == "Gelb" 
-                    or color_tag == "YELLOW" 
-                    or color_tag == "GELB"):
-                color = self.standard_colors["yellow"]
-            elif(   color_tag == "orange" 
-                    or color_tag == "Orange" 
-                    or color_tag == "ORANGE"):
-                color = self.standard_colors["orange"]
-            elif(   color_tag == "purple" 
-                    or color_tag == "Purple" 
-                    or color_tag == "lila" 
-                    or color_tag == "Lila" 
-                    or color_tag == "Pink"
-                    or color_tag == "pink"
-                    or color_tag == "PINK"   
-                    or color_tag == "PURPLE" 
-                    or color_tag == "LILA"):
-                color = self.standard_colors["purple"]
+            collected_input = text.split(" ", 1)
+            if(len(collected_input)>1):
+                retrieved_text = collected_input[1]
+                color_tag = collected_input[0]
+                #not using the new match feature from Python 3.10, since not everybody has it installed
+                if( color_tag == "red" 
+                    or color_tag == "Red" 
+                    or color_tag == "rot" 
+                    or color_tag == "Rot" 
+                    or color_tag == "ROT" 
+                    or color_tag == "RED"):
+                    color = self.standard_colors["red"]
+                elif(  color_tag == "green" 
+                        or color_tag == "Green" 
+                        or color_tag == "grün" 
+                        or color_tag == "Grün" 
+                        or color_tag == "GREEN" 
+                        or color_tag == "grun" 
+                        or color_tag == "GRÜN"):
+                    color = self.standard_colors["green"]
+                elif(   color_tag == "blue" 
+                        or color_tag == "Blue" 
+                        or color_tag == "blau" 
+                        or color_tag == "Blau" 
+                        or color_tag == "BLUE" 
+                        or color_tag == "BLAU"):
+                    color = self.standard_colors["blue"]
+                elif(   color_tag == "yellow" 
+                        or color_tag == "Yellow" 
+                        or color_tag == "gelb" 
+                        or color_tag == "Gelb" 
+                        or color_tag == "YELLOW" 
+                        or color_tag == "GELB"):
+                    color = self.standard_colors["yellow"]
+                elif(   color_tag == "orange" 
+                        or color_tag == "Orange" 
+                        or color_tag == "ORANGE"):
+                    color = self.standard_colors["orange"]
+                elif(   color_tag == "purple" 
+                        or color_tag == "Purple" 
+                        or color_tag == "lila" 
+                        or color_tag == "Lila" 
+                        or color_tag == "Pink"
+                        or color_tag == "pink"
+                        or color_tag == "PINK"   
+                        or color_tag == "PURPLE" 
+                        or color_tag == "LILA"):
+                    color = self.standard_colors["purple"]
+                else:
+                    color = [255,255,255]
+                    #add non color value to displayed text
+                    retrieved_text = color_tag +" "+ retrieved_text
             else:
                 color = [255,255,255]
-                #add non color value to displayed text
-                retrieved_text = color_tag +" "+ retrieved_text
-        else:
-            color = [255,255,255]
-            retrieved_text = collected_input[0]
+                retrieved_text = collected_input[0]
 
-        self.pixelflut.send_message(retrieved_text,color)
-        
-        now = datetime.now()
-        modified_text = '"' + retrieved_text + '" ' + "um " + now.strftime("%H:%M:%S Uhr am %d.%m.%Y")
-        #modify the main list
-        self.entries.insert(0,modified_text)
-        #remove last item, if list is too long
-        if(len(self.entries)>self.history_length):
-            self.entries.pop(-1)
+            self.pixelflut.send_message(retrieved_text,color)
             
+            now = datetime.now()
+            modified_text = '"' + retrieved_text + '" ' + "um " + now.strftime("%H:%M:%S Uhr am %d.%m.%Y")
+            #modify the main list
+            self.entries.insert(0,modified_text)
+            #remove last item, if list is too long
+            if(len(self.entries)>self.history_length):
+                self.entries.pop(-1)
+        except:
+            print("an error occured reading the string")
             
         return self.normal(entry=retrieved_text)
 
