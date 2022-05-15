687e601e2fda75bc021d4e2996ed8c8dea3718e4
Wunderbaeumchen <51451724+Wunderbaeumchen99817@users.noreply.github.com>
2022-05-15 19:00:01 +0200

    implementing Pong-Routine
    added game logic
    -> paddle-function still missing

--- Python/examples/Example-Pong.py
+++ Python/examples/Example-Pong.py
@@ -2,10 +2,13 @@ import PixelMatrix
 from math import sin, cos, pi, trunc
 from time import sleep
 import numpy as np
+from pathlib import Path
 
 import configparser
+
 config = configparser.ConfigParser() 
-config.read(r"../../../MatrixHost.ini")
+configpath = Path(__file__).parent.parent/"MatrixHost.ini" 
+config.read(configpath)
 HOST = config.get("Pixelserver","Host")
 UDP_HOST = config.get("WLED Server","Host")
 
@@ -13,9 +16,52 @@ UDP_HOST = config.get("WLED Server","Host")
 # aus Rot, Grün und Blauanteil
 Weiss = (255, 255, 255)
 Rot   = (255, 0, 0)
-Gruen = (0, 255, 0)
+Gruen = (67, 143, 73)
 Blau  = (0, 0, 255)
 Lila  = (200,100,200)
+
+#Score
+class Score():
+    left = 0
+    right = 0
+
+    def resetScore(self):
+        self.left = 0
+        self.right = 0
+
+LEFT = 0
+RIGHT = 1
+POINTS_TO_WIN = 7
+
+class ScoreDigits():
+    ZERO ="###\n# #\n###"
+    ONE =" ##\n# #\n  #"
+    TWO = "## \n # \n ##"
+    THREE = "###\n  #\n###"
+    FOUR = "# #\n###\n  #"
+    FIVE = " ##\n # \n## "
+    SIX = "#  \n## \n## "
+    SEVEN = "###\n  #\n # "
+
+    def returnString(self, value:int)-> str:
+        if value == 0:
+            return self.ZERO
+        elif value == 1:
+            return self.ONE
+        elif value == 2:
+            return self.TWO
+        elif value == 3:
+            return self.THREE
+        elif value == 4:
+            return self.FOUR
+        elif value == 5:
+            return self.FIVE
+        elif value == 6:
+            return self.SIX
+        elif value == 7:
+            return self.SEVEN
+        
+        raise ValueError("Digit is unsupported yet!")
 
 def GRAD() -> float:
     return pi/180; 
@@ -24,7 +70,7 @@ def Paddel(aArray : np.ndarray, x,y):
     for j in range(1,5):
         aArray[x+0][j+y] = Weiss
         aArray[x+1][j+y] = Weiss
-def Ball(aArray : np.ndarray, x,y):
+def Ball(aArray : np.ndarray, x:int,y:int):
     #print(x,y)
     aArray[x][y] = Weiss
     aArray[x][y+1] = Weiss
@@ -32,30 +78,173 @@ def Ball(aArray : np.ndarray, x,y):
     aArray[x+1][y+1] = Weiss
 
 
-def RotateX(x,y,a) :
+def RotateX(x,y,a) ->float:
   return x*cos(a*pi/180)#-y*sin(a*pi/180);
     
-def RotateY(x,y,a) :
+def RotateY(x,y,a) -> float:
   return x*sin(a*pi/180)#+y*cos(a*pi/180);
 
+class Letters():
+    L = "#    \n#    \n#    \n#    \n#####"
+    I = "  #  \n  #  \n  #  \n  #  \n  #  "
+    N = "#   #\n##  #\n# # #\n#  ##\n#   #"
+    K = "#  # \n# #  \n##   \n# #  \n#  # "
+    S = "#####\n#    \n#####\n    #\n#####"
+    G = "#####\n#    \n# ###\n#   #\n#####"
+    E = "#####\n#    \n###  \n#    \n#####"
+    W = "#   #\n#   #\n# # #\n## ##\n#   #"
+    T = "#####\n  #  \n  #  \n  #  \n  #  "
+    R = "###  \n#  # \n###  \n# #  \n#  # "
+    C = "#####\n#    \n#    \n#    \n#####"
+    H = "#   #\n#   #\n#####\n#   #\n#   #"
+    A = "  #  \n # # \n ### \n # # \n # # "
+    P = " ### \n # # \n ### \n #   \n #   "
+    O = "#####\n#   #\n#   #\n#   #\n#####"
+    MINUS = "     \n     \n ### \n     \n     "
+    EXCLAMATION = "  #  \n  #  \n  #  \n     \n  #  "
 
-def Flieg(Von, Speed, Winkel):
+    def returnString(self, value: str):
+        value = value.upper()
+        if(value == "L"):
+            return self.L
+        if(value == "I"):
+            return self.I
+        if(value == "N"):
+            return self.N
+        if(value == "K"):
+            return self.K
+        if(value == "S"):
+            return self.S
+        if(value == "G"):
+            return self.G
+        if(value == "E"):
+            return self.E
+        if(value == "W"):
+            return self.W
+        if(value == "T"):
+            return self.T
+        if(value == "R"):
+            return self.R
+        if(value == "C"):
+            return self.C
+        if(value == "H"):
+            return self.H
+        if(value == "!"):
+            return self.EXCLAMATION
+        if(value == "A"):
+            return self.A
+        if(value == "P"):
+            return self.P
+        if(value == "O"):
+            return self.O
+        if(value == "-"):
+            return self.MINUS
+
+        raise ValueError("Input not supported!")
+
+letters = Letters()
+
+def gameOverScreen(score : Score, matrix:PixelMatrix.UdpPixelMatrix):
+    """zeigt am Ende des Spiels den Gewinner an"""
+    game_over_array = np.zeros(shape=(Matrix.Width,Matrix.Height,3), dtype=np.uint8)
+    text = "Rechts gewinnt!"
+    #links:
+    if score.left == POINTS_TO_WIN:
+        text = "Links gewinnt!"
+    text = text.split(" ")
+
+    for j in range(len(text)):
+        for i in range(0, len(text[j])):
+            current_letter = letters.returnString(text[j][i]).split("\n")
+            for y in range(5):
+                for x in range(5):
+                    if current_letter[y][x] == "#":
+                        game_over_array[3+6*i+x][5+j*6+y] = Gruen
+
+    matrix.NdArray(game_over_array)
+    sleep(5)
+    score.resetScore()
+
+def gameStartScreen(matrix:PixelMatrix.UdpPixelMatrix):
+    """zeigt den normalen Startschirm an"""
+    start_array = np.zeros(shape=(Matrix.Width,Matrix.Height,3), dtype=np.uint8)
+    text = "Wak-pong!"
+
+    for j in range(len(text)):
+        current_letter = letters.returnString(text[j]).split("\n")
+        for y in range(5):
+            for x in range(5):
+                if current_letter[y][x] == "#":
+                    start_array[4+6*j+x][13+y] = Gruen
+
+    matrix.NdArray(start_array)
+    #TODO: add function to wait for movement of paddle
+    sleep(5)
+
+def scoredPoint(score: Score, side:int) -> bool:
+    """Fügt einen Punkt zur jeweiligen Seite hinzu
+    Gibt Wahr zurück, wenn eine Seite gewonnen hat"""
+    if side == LEFT:
+        score.left += 1
+        if score.left == POINTS_TO_WIN:
+            return True
+    elif side == RIGHT:
+        score.right += 1
+        if score.right == POINTS_TO_WIN:
+            return True
+    return False
+
+digits = ScoreDigits()
+
+def scoreDisplay(score:Score, aArray:np.ndarray):
+    """zeigt den aktuellen Score oben mittig an"""
+    #links:
+    data = digits.returnString(score.left).split("\n")
+    help_x = x_middle-5
+    for y in range(0,3):
+        for x in range(0,3):
+            if data[y][x] == "#":
+                aArray[help_x+x][y+1] = Weiss
+    
+    #rechts:
+    data = digits.returnString(score.right).split("\n")
+    help_x = x_middle+3
+    for y in range(0,3):
+        for x in range(0,3):
+            if data[y][x] == "#":
+                aArray[help_x+x][y+1] = Weiss
+
+
+def Flieg(aArray : np.ndarray ,Von, Speed, Winkel):
     x = Von[0]+RotateX(Speed,0,Winkel)
     y = Von[1]+RotateY(Speed,0,Winkel)
-    if x >= Matrix.Width-2:
-       Winkel = 180 - Winkel*1.1; 
-    if x < 0:
-       Winkel = 0 - (Winkel*1.1-180);
-    if y >= Matrix.Height-2:
-       Winkel = -90 - (Winkel-90); 
-    if y < 0:
-       Winkel = 90 - (Winkel+90);
+
+    scored = [False,None]
+
+    if x > Matrix.Width-2:
+        #pruefe, ob Ball gegen Paddel läuft
+        if (aArray[Matrix.Width-1,int(y)] == Weiss).all():
+            Winkel = 180 - Winkel*1.1
+        else:
+            scored = [True, LEFT]
+    if x < 1:
+        #pruefe, ob Ball gegen Paddel läuft
+        if (aArray[0,int(y)] == Weiss).all():
+            Winkel = 0 - (Winkel*1.1-180)
+        else:
+            scored = [True, RIGHT]
+    if y > Matrix.Height-2:
+        #pruefe, ob Ball gegen Paddel läuft
+        Winkel = -90 - (Winkel-90)
+    if y < 2:
+        #pruefe, ob Ball gegen Paddel läuft
+        Winkel = 90 - (Winkel+90)
+        
     #print(Winkel)
     x = Von[0]+RotateX(Speed,0,Winkel)
     y = Von[1]+RotateY(Speed,0,Winkel)
 
-    return [x, y], Winkel
-    
+    return [x, y], Winkel, scored    
 
 # Wir erzeugen eine Instanz Namens ,,Matrix``
 # der Klasse PixelMatrix() aus der PixelMatrix Bibliothek
@@ -73,16 +262,51 @@ sleep(1.6)
 x = 30
 y = 16
 
+x_middle = int(Matrix.Width/2)
+y_middle = int(Matrix.Height/2)
+score = Score()
 
-position = [15.0, 15.0]
-angle    = 99
+class Position():
+    position = [x_middle, y_middle-1]
+    angle = 20
+
+    def resetPosition(self):
+        self.position = [x_middle, y_middle-1]
+        self.angle = 20
+
+class PaddelData():
+    left = y_middle-3
+    right = y_middle-3
+
+    def resetPaddel(self):
+        self.left = y_middle-3
+        self.right = y_middle-3
+
+paddel = PaddelData()
+position_class = Position()
+
 for i in range(1,1000):
     Spielfeld = np.copy(Background)
-    Ball(Spielfeld,trunc(position[0]),trunc(position[1]))
-    Paddel(Spielfeld,0,5)
-    Paddel(Spielfeld,Matrix.Width-2,5)
-    position, angle = Flieg(position, 1, angle)
-    Matrix.NdArray(Spielfeld)
-    #sleep(0.05)
+    scoreDisplay(score, Spielfeld)
+    Paddel(Spielfeld,0,paddel.left)
+    Paddel(Spielfeld,Matrix.Width-2,paddel.right)
+    Ball(Spielfeld,trunc(position_class.position[0]),trunc(position_class.position[1]))
+    position_class.position, position_class.angle, scored = Flieg(Spielfeld, position_class.position, 1, position_class.angle)
+    if scored[0]:
+        #pruefe, ob Spiel zu Ende ist
+        if(scoredPoint(score ,scored[1])):
+            #beende Spiel
+            gameOverScreen(score, Matrix)
+            gameStartScreen(Matrix)
+            position_class.resetPosition()
+            paddel.resetPaddel()
+        else:
+            #resette Spielfeld für naechsten Turn
+            position_class.resetPosition()
+            paddel.resetPaddel()
+            
+    else:
+        Matrix.NdArray(Spielfeld)
+        #sleep(0.05)
 
 
