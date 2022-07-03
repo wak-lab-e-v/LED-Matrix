a49b93fc268340273e29f134cfd8c738bf15b726
Martin <mhilde@gmx.de>
2022-07-03 23:38:19 +0200

    Pong mit Controller und zufälligem Start, sowie Start in Abhängigkeit wer zuletzt verloren hat

--- Client/examples/Example-Pong.py
+++ Client/examples/Example-Pong.py
@@ -6,6 +6,8 @@ from pathlib import Path
 import keyboard
 import configparser
 from functools import wraps, lru_cache
+import serial
+from random import random
 
 config = configparser.ConfigParser() 
 configpath = Path(__file__).parent.parent/"MatrixHost.ini"
@@ -21,6 +23,55 @@ Rot   = (255, 0, 0)
 Gruen = (67, 143, 73)
 Blau  = (0, 0, 255)
 Lila  = (200,100,200)
+
+def crc16_(data: bytes):
+    xor_in = 0x0000  # initial value
+    xor_out = 0x0000  # final XOR value
+    poly = 0x1021  # generator polinom (normal form)
+
+    reg = xor_in
+    for octet in data:
+        # reflect in
+        for i in range(8):
+            topbit = reg & 0x8000
+            if octet & (0x80 >> i):
+                topbit ^= 0x8000
+            reg <<= 1
+            if topbit:
+                reg ^= poly
+        reg &= 0xFFFF
+        # reflect out
+    return reg ^ xor_out
+
+def findPort():
+    # Find first available EiBotBoard by searching USB ports.
+    # Return serial port object.
+    try:
+        from serial.tools.list_ports import comports
+    except ImportError:
+        return None
+    if comports:
+        try:
+            com_ports_list = list(comports())
+        except TypeError: # https://github.com/evil-mad/plotink/issues/38
+            return None
+        serial_portname = None
+        for port in com_ports_list:
+                print(str(port[2]))
+                if port[2].startswith("USB VID:PID=0403:6001"): #FDTI 
+                    serial_portname = port[0]  # Success; EBB found by VID/PID match.
+                    break  # stop searching-- we are done.
+        if serial_portname is None:
+            for port in com_ports_list:
+                #inkex.errormsg(gettext.gettext(str(port[1])))
+                print(port[1])
+                if port[1].startswith("USB-SERIAL CH340"): # Nano
+                    serial_portname = port[0]  # Success; EBB found by name match.
+                    break  # stop searching-- we are done.
+
+        return serial_portname
+
+    
 
 #Score
 class Score():
@@ -229,21 +280,21 @@ def Flieg(aArray : np.ndarray ,Von, Speed, Winkel):
 
     if x > Matrix.Width-2:
         #pruefe, ob Ball gegen Paddel läuft
-        if (aArray[Matrix.Width-1,int(y)] == Weiss).all():
+        if (aArray[Matrix.Width-1,int(y)] == Weiss).all() or (aArray[Matrix.Width-1,int(y+1)] == Weiss).all():
             Winkel = 180 - Winkel*1.1
         else:
             scored = [True, LEFT]
-    if x < 1:
+    if x < 2:
         #pruefe, ob Ball gegen Paddel läuft
-        if (aArray[0,int(y)] == Weiss).all():
+        if (aArray[1,int(y)] == Weiss).all() or (aArray[1,int(y+1)] == Weiss).all():
             Winkel = 0 - (Winkel*1.1-180)
         else:
             scored = [True, RIGHT]
     if y > Matrix.Height-2:
-        #pruefe, ob Ball gegen Paddel läuft
+        #pruefe, ob Ball gegen die Decke
         Winkel = -90 - (Winkel-90)
     if y < 2:
-        #pruefe, ob Ball gegen Paddel läuft
+        #pruefe, ob Ball gegen den Boden
         Winkel = 90 - (Winkel+90)
         
     #print(Winkel)
@@ -273,12 +324,18 @@ y_middle = int(Matrix.Height/2)
 score = Score()
 
 class Position():
-    position = [x_middle, y_middle-1]
-    angle = 20
 
-    def resetPosition(self):
-        self.position = [x_middle, y_middle-1]
-        self.angle = 20
+    def __init__(self):
+        self.resetPosition(random() > 0.5)
+
+    def resetPosition(self, Dir = False):
+        if Dir:
+            self.position = [x_middle +10, y_middle-1]
+        else:
+            self.position = [x_middle-10, y_middle-1]
+        self.angle = -30 + 60*random()
+        if Dir:
+            self.angle += 180
 
 class PaddelData():
     left = y_middle-3
@@ -288,54 +345,110 @@ class PaddelData():
         self.left = y_middle-3
         self.right = y_middle-3
 
-paddel = PaddelData()
-position_class = Position()
+SerPosLinks = 0
+SerPosRechts = 0
+SerPosLinksOffset = 0
+SerPosRechtsOffset = 0
 
-#for i in range(1,1000):
-lastSend = time()
-while 1:
+def SerPos(serial):
+    global SerPosLinks
+    global SerPosRechts
+    global SerPosLinksOffset
+    global SerPosRechtsOffset
+    while(len(data := ser.read(1)) > 0):
+        if (len(data) > 0) and (data[0] == 0xAD):
+            data = ser.read(4)
+            if len(data) == 4:
+                Paket = bytes(b'\xad' + bytearray(data[:2]))
+                myCrc16 = crc16_(Paket)
+                if myCrc16 == int.from_bytes(data[2:4], "little"):
+                    SerPosLinks = SerPosLinksOffset + (data[0] >> 1)
+                    SerPosRechts = SerPosRechtsOffset + (data[1] >> 1)
+                    if (SerPosLinks > 28):
+                        SerPosLinksOffset = SerPosLinksOffset - (SerPosLinks - 28)
+                        SerPosLinks = SerPosLinksOffset + (data[0] >> 1)
+
+                    if (SerPosLinks < 0):
+                        SerPosLinksOffset = SerPosLinksOffset - (SerPosLinks)
+                        SerPosLinks = SerPosLinksOffset + (data[0] >> 1)
+
+                    if (SerPosRechts > 28):
+                        SerPosRechtsOffset = SerPosRechtsOffset - (SerPosRechts - 28)
+                        SerPosRechts = SerPosRechtsOffset + (data[1] >> 1)
+
+                    if (SerPosRechts < 0):
+                        SerPosRechtsOffset = SerPosRechtsOffset - (SerPosRechts)
+                        SerPosRechts = SerPosRechtsOffset + (data[1] >> 1)
+                        
+                    print(' '.join('{:02x}'.format(x) for x in data[0:2]))
+                    print(SerPosLinks, SerPosRechts, SerPosLinksOffset, SerPosRechtsOffset)
     
-    if keyboard.is_pressed("p"):
-        if paddel.right > 0:
-            paddel.right -= 1
-    if keyboard.is_pressed("l"):
-        if paddel.right < Matrix.Height-5:
-            paddel.right += 1
 
-    if keyboard.is_pressed("q"):
-        if paddel.left > 0:
-            paddel.left -= 1
-    if keyboard.is_pressed("a"):
-        if paddel.left < Matrix.Height-5:
-            paddel.left += 1
-    sleep(0.04)
-    delay = time() - lastSend
-    if (delay > 0.080) :
+if __name__ == '__main__':
+    paddel = PaddelData()
+    position_class = Position()
+    if (port := findPort()):
+        ser = serial.Serial()
+        ser.port = port
+        ser.baudrate = 4800
+        ser.timeout = 0.05
+        ser.open()
+        sleep(0.15)
+        ser.flushInput()
+        sleep(3.15)
+        ser.write('X'.encode('ANSI')) # deaktivieren wenn möglich den 433MHz Sender
         
-        lastSend = time()
-            
-        Spielfeld = np.copy(Background)
-        scoreDisplay(score, Spielfeld)
-        Paddel(Spielfeld,0,paddel.left)
-        Paddel(Spielfeld,Matrix.Width-2,paddel.right)
-    
-        Ball(Spielfeld,trunc(position_class.position[0]),trunc(position_class.position[1]))
-        position_class.position, position_class.angle, scored = Flieg(Spielfeld, position_class.position, 1, position_class.angle)
-        if scored[0]:
-            #pruefe, ob Spiel zu Ende ist
-            if(scoredPoint(score ,scored[1])):
-                #beende Spiel
-                gameOverScreen(score, Matrix)
-                gameStartScreen(Matrix)
-                position_class.resetPosition()
-                paddel.resetPaddel()
-            else:
-                #resette Spielfeld für naechsten Turn
-                position_class.resetPosition()
-                paddel.resetPaddel()
-                
+
+    #for i in range(1,1000):
+    lastSend = time()
+    while 1:
+        if (port == None):
+            if keyboard.is_pressed("p"):
+                if paddel.right > 0:
+                    paddel.right -= 1
+            if keyboard.is_pressed("l"):
+                if paddel.right < Matrix.Height-5:
+                    paddel.right += 1
+
+            if keyboard.is_pressed("q"):
+                if paddel.left > 0:
+                    paddel.left -= 1
+            if keyboard.is_pressed("a"):
+                if paddel.left < Matrix.Height-5:
+                    paddel.left += 1
         else:
-            Matrix.NdArray(Spielfeld)
+            SerPos(ser)
+            paddel.left = SerPosLinks
+            paddel.right = SerPosRechts
+            
+        sleep(0.04)
+        delay = time() - lastSend
+        if (delay > 0.080) :
+            
+            lastSend = time()
+                
+            Spielfeld = np.copy(Background)
+            scoreDisplay(score, Spielfeld)
+            Paddel(Spielfeld,0,paddel.left)
+            Paddel(Spielfeld,Matrix.Width-2,paddel.right)
+        
+            Ball(Spielfeld,trunc(position_class.position[0]),trunc(position_class.position[1]))
+            position_class.position, position_class.angle, scored = Flieg(Spielfeld, position_class.position, 1, position_class.angle)
+            if scored[0]:
+                #pruefe, ob Spiel zu Ende ist
+                if(scoredPoint(score ,scored[1])):
+                    #beende Spiel
+                    gameOverScreen(score, Matrix)
+                    gameStartScreen(Matrix)
+                    position_class.resetPosition()
+                    paddel.resetPaddel()
+                else:
+                    #resette Spielfeld für naechsten Turn
+                    position_class.resetPosition(scored[1] != LEFT)
+                    paddel.resetPaddel()
+                    
+            else:
+                Matrix.NdArray(Spielfeld)
        
         
         
